7057668d96752976ddee90ee60a29f39f75e7f4a
Akis <b_kalogeropoulos@yahoo.gr>
2021-10-07 19:00:00 +0300

    Style the App

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,7 +4,7 @@ import ResultDetail from "./ResultsDetail";
 
 const ResultsList = ({title, results}) => {
     return(
-        <View>
+        <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
             <FlatList 
                 horizontal
@@ -22,9 +22,13 @@ const ResultsList = ({title, results}) => {
 
 const styles = StyleSheet.create({
     title: {
-        fontSize: 18,
+        fontSize: 20,
         fontWeight:'bold',
-        marginLeft: 15
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    container: {
+        marginBottom: 10
     }
 });
 
