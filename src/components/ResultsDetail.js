13c0acfca6c7445be71baa8210743bbd8cc38ef1
Akis <b_kalogeropoulos@yahoo.gr>
2021-10-07 20:34:00 +0300

    fix the names if the result is null

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -22,7 +22,8 @@ const styles = StyleSheet.create({
         width:250,
         height: 120,
         borderRadius: 4,
-        marginBottom: 5
+        marginBottom: 5,
+        marginRight: 15
     },
     nameStyle: {
         fontWeight: 'bold',
