0200f376505490f4d6fe6e70903f0e3db274a13a
Akis <b_kalogeropoulos@yahoo.gr>
2021-10-07 19:15:45 +0300

    more style

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
 import ResultsList from "../components/ResultsList";
@@ -15,36 +15,39 @@ const SearchScreen = () => {
     };
 
     return (
-        <View>
+        <>
             <SearchBar 
                 term={term}
                 onTermChange={setTerm}
                 onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length} results</Text>
-            <ResultsList 
-                title="Cost Effective"
-                results={filterResultByPrice('$')}
-            />
-            <ResultsList 
-                title="Bit Pricier"
-                results={filterResultByPrice('$$')}
-            />
-            <ResultsList 
-                title="Big Spender"
-                results={filterResultByPrice('$$$')}
-            />
-            <ResultsList 
-                title="Very Big Spender"
-                results={filterResultByPrice('$$$$')}
-            />
-        </View>
+            <ScrollView>
+                <ResultsList 
+                    title="Cost Effective"
+                    results={filterResultByPrice('$')}
+                />
+                <ResultsList 
+                    title="Bit Pricier"
+                    results={filterResultByPrice('$$')}
+                />
+                <ResultsList 
+                    title="Big Spender"
+                    results={filterResultByPrice('$$$')}
+                />
+                <ResultsList 
+                    title="Very Big Spender"
+                    results={filterResultByPrice('$$$$')}
+                />
+            </ScrollView>
+        </>
     );
 };
 
 const styles = StyleSheet.create({
-
+    view: {
+        flex: 1
+    }
 });
 
 export default SearchScreen;
